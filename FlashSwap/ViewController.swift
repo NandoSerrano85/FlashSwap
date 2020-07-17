1ca45e40a13bf16bb9238259619a2283d0dc5c51
Fernando Serrano <fserr009@fiu.edu>
2020-07-17 18:32:01 -0400

    Initial Commit

---
+++ FlashSwap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlashSwap
+//
+//  Created by Fernando Serrano on 7/17/20.
+//  Copyright © 2020 FIU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
